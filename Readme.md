1331664548505cb82d565f49b5d09ab8e9f49290
Chris <chris.hulbert@gmail.com>
2026-02-11 18:36:25 +1100

    Load pngs

--- Readme.md
+++ Readme.md
@@ -1,14 +1,20 @@
 # ScaleFX-rs
 
-ScaleFX pixel art upcaler, for the CPU, in Rust.
+ScaleFX pixel art upcaler, on the CPU (not a shader!), in Rust.
 
-![Sample](https://github.com/chrishulbert/cpu-scalefx-rs/raw/main/readme/sample.png)
+![Blooguard](https://github.com/chrishulbert/cpu-scalefx-rs/raw/main/readme/Blooguard.png)
 
-![Big](https://github.com/chrishulbert/cpu-scalefx-rs/raw/main/readme/out.big.png)
+![Blooguard](https://github.com/chrishulbert/cpu-scalefx-rs/raw/main/readme/Blooguard.big.png)
 
-![Bigger](https://github.com/chrishulbert/cpu-scalefx-rs/raw/main/readme/out.bigger.png)
+To use: `cargo run in.png out.png`
+
+Original shader algorithm thanks to Sp00kyFox, 2016.
 
 ## Why?
+
+![Council](https://github.com/chrishulbert/cpu-scalefx-rs/raw/main/readme/Council.png)
+
+![Council](https://github.com/chrishulbert/cpu-scalefx-rs/raw/main/readme/Council.big.png)
 
 ScaleFX already exists as a far-faster GPU shader, so why this project? Fair question. My reasons:
 
@@ -16,11 +22,21 @@ ScaleFX already exists as a far-faster GPU shader, so why this project? Fair que
 * For offline upscaling of assets, as opposed to realtime.
 * Looks better than XBRZ!
 
-## How to use this
+## How to use this in your code
+
+![RoboRed](https://github.com/chrishulbert/cpu-scalefx-rs/raw/main/readme/RoboRed.png)
+
+![RoboRed](https://github.com/chrishulbert/cpu-scalefx-rs/raw/main/readme/RoboRed.big.png)
 
 Simply copy `scalefx.rs` into your project, and call `scalefx::scale3x(width, height, pixels)`, where pixels is a slice of u32, containing 0xRRGGBBAA data.
 
 If anybody out there actually uses this, we can have a conversation about uplifting this into a proper crate :) 
+
+## Examples
+
+![SQ3](https://github.com/chrishulbert/cpu-scalefx-rs/raw/main/readme/sq3.fixed.png)
+
+![SQ3](https://github.com/chrishulbert/cpu-scalefx-rs/raw/main/readme/sq3.fixed.big.png)
 
 ## References
 
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,71 @@
-mod png;
 mod scalefx;
 
-fn sample() -> (usize, usize, Vec<u32>) {
-    let t = 0; // Transparent.
-    let b = 0xff; // Black.
-    let y = 0xffff00ff; // Yellow.
-    let g = 0x00ff00ff; // Green.
-    let l = 0x0000ffff; // bLue.
-    let w = 0xffffffff; // White.
-    let e = 0x888888ff; // grEy.
-
-    (12, 12, vec![
-        t,t,t,b,b,b,b,b,b,t,t,t,
-        t,t,b,y,g,g,w,g,y,b,t,t,
-        t,b,y,y,y,y,g,w,g,y,b,t,
-        b,g,e,y,y,y,g,w,g,y,y,b,
-        b,y,y,y,y,y,g,w,g,y,y,b,
-        b,y,g,y,e,e,e,e,e,e,y,b,
-        b,e,g,y,w,l,w,w,l,w,y,b,
-        b,y,y,y,w,w,e,e,w,w,y,b,
-        b,y,y,e,w,w,w,w,w,w,e,b,
-        t,b,e,e,e,w,w,w,w,e,b,t,
-        t,t,b,b,e,e,e,e,b,b,t,t,
-        t,t,t,t,b,e,e,b,t,t,t,t,
-    ])
-}
+use png::{self, BitDepth, ColorType, Transformations};
 
 fn main() {
-    let (width, height, pixels) = sample();
-    std::fs::write("sample.png", png::png_data(width, height, &pixels)).unwrap();
+    println!("-=[ ScaleFX-rs Pixel Art Upscaler ]=-");
 
-    let (width, height, pixels) = scalefx::scale3x(width, height, &pixels);
-    std::fs::write("out.big.png", png::png_data(width, height, &pixels)).unwrap();
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 3 {
+        println!("Usage:");
+        println!("scalefx in.png out.png");
+    } else {
+        upscale(&args[1], &args[2]);
+    }
 
-    let (width, height, pixels) = scalefx::scale3x(width, height, &pixels);
-    std::fs::write("out.bigger.png", png::png_data(width, height, &pixels)).unwrap();
+    // let (width, height, pixels) = sample();
+    // std::fs::write("sample.png", png::png_data(width, height, &pixels)).unwrap();
+
+    // let (width, height, pixels) = scalefx::scale3x(width, height, &pixels);
+    // std::fs::write("out.big.png", png::png_data(width, height, &pixels)).unwrap();
+
+    // let (width, height, pixels) = scalefx::scale3x(width, height, &pixels);
+    // std::fs::write("out.bigger.png", png::png_data(width, height, &pixels)).unwrap();
+}
+
+fn upscale(in_path: &str, out: &str) {
+    let (width, height, pixels) = load_png(in_path);
+    println!("Scaling...");
+    let (width, height, pixels) = scalefx::scale9x(width, height, &pixels);
+    println!("Scaled to: {} x {}", width, height);
+    save_png(width, height, &pixels, out);
+}
+
+fn load_png(path: &str) -> (usize, usize, Vec<u32>) {
+    println!("Loading: {}", path);
+    let in_file = std::fs::File::open(path).expect("Failed to open file!");
+    let in_reader = std::io::BufReader::new(in_file);
+    let mut decoder = png::Decoder::new(in_reader);
+    decoder.set_transformations(Transformations::ALPHA); // Auto-converts to 8-bit RGBA.
+    let mut reader = decoder.read_info().expect("Failed to read header");
+    let mut buf = vec![0; reader.output_buffer_size().unwrap()];
+    let info = reader.next_frame(&mut buf).expect("Failed to decode");
+    let bytes = &buf[..info.buffer_size()];
+    assert_eq!(info.bit_depth, BitDepth::Eight);
+    assert_eq!(info.color_type, ColorType::Rgba);
+    // Convert to RGBA
+    let mut rgbas: Vec<u32> = Vec::with_capacity(info.width as usize * info.height as usize);
+    for chunk in bytes.chunks_exact(4) {
+        rgbas.push(u32::from_be_bytes(chunk.try_into().unwrap()));
+    }
+    println!("Loaded: {} x {} px", info.width, info.height);
+    (info.width as usize, info.height as usize, rgbas)
+}
+
+fn save_png(width: usize, height: usize, pixels: &[u32], path: &str) {
+    println!("Saving: {}", path);
+    let file = std::fs::File::create(path).unwrap();
+    let ref mut buf_writer = std::io::BufWriter::new(file);
+    let mut encoder = png::Encoder::new(buf_writer, width as u32, height as u32);
+    encoder.set_color(png::ColorType::Rgba);
+    encoder.set_depth(png::BitDepth::Eight);
+    let mut writer = encoder.write_header().unwrap();
+    let mut data: Vec<u8> = Vec::with_capacity(width * height * 4);
+    for p in pixels {
+        data.push((p >> 24) as u8);
+        data.push((p >> 16) as u8);
+        data.push((p >> 8) as u8);
+        data.push(*p as u8);
+    }
+    writer.write_image_data(&data).unwrap(); // Save
 }
